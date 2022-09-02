46d231dee7a5285e80109787bbe109f51eec5515
ungnome <73322863+ungnome@users.noreply.github.com>
2022-09-02 19:22:09 -0400

    chore: initial commit

---
+++ release.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      "@semantic-release/git",
+      { message: "chore(release): ${nextRelease.version}" },
+    ],
+    "@semantic-release/github",
+  ],
+};
